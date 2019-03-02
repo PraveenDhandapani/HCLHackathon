95804f6b0afd2178f2182cc65d20f774a87c4f98
user <user@DESKTOP-3I2S5D4>
2019-03-02 00:14:33 -0800

    Contact details

---
+++ rbs/src/main/java/com/rbshcl/databreach/model/ContactDetails.java
@@ -0,0 +1,69 @@
+package com.rbshcl.databreach.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ContactDetails {
+	
+	private String fullName;
+	
+	private Long Age;
+	
+	private String gender;
+	
+	private String maritalStatus;
+	
+	private String dependent;
+	
+	private String nextOfKin;
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Long getAge() {
+		return Age;
+	}
+
+	public void setAge(Long age) {
+		Age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+
+	public void setMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+
+	public String getDependent() {
+		return dependent;
+	}
+
+	public void setDependent(String dependent) {
+		this.dependent = dependent;
+	}
+
+	public String getNextOfKin() {
+		return nextOfKin;
+	}
+
+	public void setNextOfKin(String nextOfKin) {
+		this.nextOfKin = nextOfKin;
+	}
+	
+	
+}
