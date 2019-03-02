8c94c7d44045b18a8e96fa393010ea9833461e9b
praveen dhandapani <praveen.2471993@gmail.com>
2019-03-01 21:57:24 -0800

    framework code

---
+++ rbs/src/main/java/com/hackathon/rbs/RbsApplication.java
@@ -0,0 +1,14 @@
+package com.hackathon.rbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbsApplication.class, args);
+		System.out.println("Welcome");
+	}
+
+}
