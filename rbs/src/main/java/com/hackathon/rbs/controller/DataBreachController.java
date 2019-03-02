86a7ee87a62fbc448bf194566908b7ecf27302ea
user <user@DESKTOP-3I2S5D4>
2019-03-02 02:55:43 -0800

    post and get main

--- rbs/src/main/java/com/hackathon/rbs/controller/DataBreachController.java
+++ rbs/src/main/java/com/hackathon/rbs/controller/DataBreachController.java
@@ -2,13 +2,17 @@ package com.hackathon.rbs.controller;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.hackathon.rbs.model.DataBreachDetails;
 import com.hackathon.rbs.repository.DataBreachRepository;
@@ -30,8 +34,8 @@ public class DataBreachController {
 	
 	}
 	
-	@GetMapping("/{riskProfile}/data-breach-details") 
-	public List<DataBreachDetails> getDataBreachDetailsByRisk(String riskProfile) {
+	@RequestMapping(value="/{riskProfile}/data-breach-details", method = RequestMethod.GET, produces = {"application/json"})
+	public List<DataBreachDetails> getDataBreachDetailsByRisk(@PathVariable("riskProfile") String riskProfile) {
 		return service.getAllDataBreachDetailsByRisk(riskProfile);
 	}
 	
